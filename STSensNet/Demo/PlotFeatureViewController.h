1f7d2b1302fa0e90b2fdf91cf5c26252967ee170
Lorenzo Invidia <loreinvidia@gmail.com>
2017-11-26 19:27:47 +0100

    Created segues to Plot View
    Added:
    
    - Plot Feature View Controller in Demoview storyboard
    - Sensor Fusion View Controller in Demoview storyboard
    - Segues to plot view

---
+++ STSensNet/Demo/PlotFeatureViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlotFeatureViewController.h
+//  STSensNet
+//
+//  Created by Lorenzo Invidia on 26/11/2017.
+//  Copyright © 2017 STCentralLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlotFeatureViewController : UIViewController
+
+@end
