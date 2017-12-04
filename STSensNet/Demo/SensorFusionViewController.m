c24dfc8e57bba14342beee3507d1a3e3d10ad637
Lorenzo Invidia <loreinvidia@gmail.com>
2017-12-04 20:44:17 +0100

    test
    test

--- STSensNet/Demo/SensorFusionViewController.m
+++ STSensNet/Demo/SensorFusionViewController.m
@@ -15,7 +15,7 @@
 @implementation SensorFusionViewController
 @synthesize featureLabel, featureLabelText;
 
-<<<<<<< HEAD
+
 BlueSTSDKFeatureMemsSensorFusion *mSensorFusionFeature;
 GLKQuaternion mQuatReset;
 SCNScene *mScene;
@@ -98,26 +98,5 @@ SCNNode *mObjectNode;
 //    if(feature == mSensorFusionFeature)
 //        [self updateRotation:sample];
 //}
-=======
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    self.featureLabel.text = featureLabelText;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
->>>>>>> parent of 0652255... Basic SensorFusion implementation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
